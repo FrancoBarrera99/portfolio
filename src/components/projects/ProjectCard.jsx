464f46c4b9f61b977777846b3cc7ddb4ac5c887c
FrancoBarrera99 <56893886+FrancoBarrera99@users.noreply.github.com>
2022-08-04 12:54:35 -0300

    projects component

---
+++ src/components/projects/ProjectCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { FaLink } from 'react-icons/fa'
+
+import './ProjectCard.scss'
+
+const ProjectCard = ({src, name, link}) => {
+
+  return (
+    <div className='projectcard_container' onClick={() => {window.open(link)}}>
+        <div className="img_container">
+            <img className='card_img' src={src} alt="project" />
+        </div>
+        <h4 className='card_title'>{name}</h4>
+        <div className='card_link_icon'><FaLink/></div>
+    </div>
+  )
+}
+
+export default ProjectCard
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+
+import Image from '../../assets/test.png'
+
+import './Projects.scss'
 
 const Projects = () => {
+
+  const projects = [
+    {
+      id:1,
+      src: Image,
+      name: "Projecto 1",
+      link: "https://www.google.com.ar"
+    },
+    {
+      id:2,
+      src: Image,
+      name: "Projecto 2",
+      link: "https://www.google.com.ar"
+    },
+    {
+      id:3,
+      src: Image,
+      name: "Projecto 3",
+      link: "https://www.google.com.ar"
+    },
+    {
+      id:4,
+      src: Image,
+      name: "Projecto 4",
+      link: "https://www.google.com.ar"
+    },
+    {
+      id:5,
+      src: Image,
+      name: "Projecto 5",
+      link: "https://www.google.com.ar"
+    },
+  ]
+
   return (
-    <div>Projects</div>
+    <div className='projects_container'>
+      <h1 className='projects_title'>My recent work</h1>
+      <div className='cards_grid'>
+        {projects.map((project) => {
+            return (<ProjectCard 
+            key={project.id}
+            src={project.src}
+            name={project.name}
+            link={project.link}
+            />)
+          })}
+      </div>
+    </div>
   )
 }
 
